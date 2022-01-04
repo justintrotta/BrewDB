32f5a8ad1de3494e0c4522ba947190fcb491aa12
Justin <justindavidtrotta@gmail.com>
2022-01-03 20:34:55 -0500

    finished search handler

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let map;
+let markers = [];
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 	40.730610, lng: -73.935242},
-    zoom: 8,
+    zoom: 10,
   });
 }
 
@@ -14,10 +15,11 @@ function initMap() {
     console.log(BEER_FETCH)
     console.log(BREW_FETCH)
 
-    fetchBrewContent();
-    searchHandler();
-    // formSwapHandler();
-    
+    document.addEventListener('DOMContentLoaded', () => {
+        fetchBrewContent();
+        searchHandler();
+        // formSwapHandler();
+    })
 
     function fetchBeerContent() {
         BEER_FETCH.then(o => o.forEach(renderList))
@@ -28,7 +30,7 @@ function initMap() {
     }
     
     function renderListBrew(obj) {
-        const list = document.querySelector("div#menu-panel");
+        const list = document.querySelector("div#menu-container");
         const caption = document.createElement("div");
         caption.className = "caption";
 
@@ -49,28 +51,40 @@ function initMap() {
 
         const location = {lat: parseFloat(obj.latitude), lng: parseFloat(obj.longitude)}
 
-        const marker = new google.maps.Marker({
+        let marker = new google.maps.Marker({
+            title: obj.name,
             position: location,
             map: map
         })
 
-        console.log(location)
+        markers.push(marker)
 
         item.addEventListener("click", (e) => {
 
         })
+
     }
 
     function searchHandler() {
-        let searchBar = document.querySelector("form#search");
+        let searchBar = document.querySelector("form");
         console.log(searchBar)
         searchBar.addEventListener("submit", (e) => {
             e.preventDefault();
-            let cityName = e.target.value;
-            cityName = cityName.toLowerCase();
-            let cityArr = cityName.split(" ");
-            cityName = cityArr.join("_");
-            console.log(cityName);
+            let cityName = e.target[0].value;
+            let cityLower = cityName.toLowerCase();
+            let cityArr = cityLower.split(" ");
+            let cityUrl = cityArr.join("_");
+            const cityFetch = fetch (BREW_URL + `?by_city=${cityUrl}`).then(r => r.json());
+            clearList();
+            removeMarkers();
+            cityFetch.then(o => o.forEach(renderListBrew))
+            cityFetch.then(o => {
+                    let lat = parseFloat(o[0].latitude);
+                    let lng = parseFloat(o[0].longitude);
+                    console.log(lat, lng);
+                    let location = {lat: lat, lng: lng};
+                    map.panTo(location)
+                })
         })
     }
 
@@ -88,13 +102,18 @@ function initMap() {
     //     })
     // }
 
-    function clearBody(){
+    function clearBody() {
         const bodyContent = document.querySelector("div#content-section");
         while(bodyContent.firstChild) {bodyContent.removeChild(bodyContent.firstChild)};
     }
 
-    function clearList(){
-        const listContent = document.querySelector("div#menu-panel");
+    function clearList() {
+        const listContent = document.querySelector("div#menu-container");
         while(listContent.firstChild) {listContent.removeChild(listContent.firstChild)};
     }
 
+    function removeMarkers() {
+        for (i=0; i<markers.length; i++) {
+            markers[i].setMap(null)
+        }
+    }
