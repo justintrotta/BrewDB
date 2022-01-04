34bf33e930808f30ad57f58c6a59621b4fb0e50c
Daniel <dkilcullen25@gmail.com>
2022-01-04 10:41:37 -0600

    completed beverage search functionality

--- index.js
+++ index.js
@@ -8,174 +8,187 @@ function initMap() {
   });
 }
 
-    BEER_URL = "https://api.punkapi.com/v2/beers"
-    BREW_URL = "https://api.openbrewerydb.org/breweries"
-    const BEER_FETCH = fetch(BEER_URL).then(r => r.json());
-    const BREW_FETCH = fetch(BREW_URL).then(r => r.json());
-    console.log(BEER_FETCH)
-    console.log(BREW_FETCH)
+BEER_URL = "https://api.punkapi.com/v2/beers"
+BREW_URL = "https://api.openbrewerydb.org/breweries"
+const BEER_FETCH = fetch(BEER_URL).then(r => r.json());
+const BREW_FETCH = fetch(BREW_URL).then(r => r.json());
 
-    document.addEventListener('DOMContentLoaded', () => {
-        fetchBrewContent();
-        searchHandler();
-        // formSwapHandler();
+document.addEventListener('DOMContentLoaded', () => {
+    fetchBrewContent();
+    // searchHandler();
+
+    document.querySelector('form#search')[0].addEventListener('change', formSwapHandler )
+    document.querySelector('form#search')[1].addEventListener('change', formSwapHandler )
+    document.querySelector("form#search").addEventListener('submit',searchHandler)
+})
+
+function fetchBeerContent() {
+    BEER_FETCH.then(o => o.forEach(renderListBeer))
+}
+
+function renderListBeer(obj) {
+    const list = document.querySelector("div#menu-container");
+    const caption = document.createElement("div");
+    caption.className = "caption";
+
+    const item = document.createElement("div");
+    item.className = "menu-block";
+    item.id = obj.id
+
+    const name = document.createElement("div");
+    name.className = "title";
+    name.innerHTML = `${obj.name} (ABV ${obj.abv}%)`;
+
+    const subtitle = document.createElement("div");
+    subtitle.className = "subtitle";
+    subtitle.innerHTML = obj.tagline
+    console.log('hello')
+
+    caption.append(name, subtitle);
+    item.append(caption);
+    item.classList.add('menu-block');
+    list.append(item);
+}
+
+function fetchBrewContent() {
+    BREW_FETCH.then(o => o.forEach(renderListBrew))
+}
+
+function renderListBrew(obj) {
+    const list = document.querySelector("div#menu-container");
+    const caption = document.createElement("div");
+    caption.className = "caption";
+
+    const item = document.createElement("div");
+    item.className = "menu-block";
+    item.id = obj.id
+
+    const name = document.createElement("div");
+    name.className = "title";
+    name.innerHTML = obj.name;
+
+    const subtitle = document.createElement("div");
+    subtitle.className = "subtitle";
+    subtitle.innerHTML = `${obj.city}, ${obj.state}`;
+
+    caption.append(name, subtitle);
+    item.append(caption);
+    item.classList.add('menu-block');
+    list.append(item);
+
+    const location = {lat: parseFloat(obj.latitude), lng: parseFloat(obj.longitude)}
+
+    let marker = new google.maps.Marker({
+        title: obj.name,
+        position: location,
+        map: map
     })
 
-    function fetchBeerContent() {
-        BEER_FETCH.then(o => o.forEach(renderList))
-    }
+    markers.push(marker)
 
-    function fetchBrewContent() {
-        BREW_FETCH.then(o => o.forEach(renderListBrew))
-    }
+    //when user clicks on a search result, populate details section
+    //also pan to result in the map view
+    item.addEventListener("click", fetchBrewDetails)
+}
 
-    //used to fetch add'l details for content section
-    function fetchBrewDetails(e) {
-            console.log(e.target)
-            fetch(BREW_URL + `/${e.target.id}`)
-            .then(res => res.json())
-            .then(obj => {
-                const name = document.querySelector('#detail-name')
-                name.textContent = obj.name
+function searchHandler(e) {
+    e.preventDefault()
 
-                const type = document.querySelector('#detail-type')
-                type.textContent = obj.brewery_type
-
-                const address = document.querySelector('#detail-address')
-                address.textContent = obj.street
-
-                const cityState = document.querySelector('#detail-city-state')
-                cityState.textContent = `${obj.city}, ${obj.state}`
-
-                const phone = document.querySelector('#detail-phone')
-                phone.textContent = obj.phone
-
-                const website = document.querySelector('#detail-website')
-                website.textContent = obj.website_url
-
-                try { 
-                    map.panTo({lat: parseFloat(obj.latitude), lng: parseFloat(obj.longitude)})
-                } catch {
-                    alert('No geolocation data available for this brewery.')
-                }
-                
-            })
-    }
+    let searchBar = document.querySelector("form");
     
-    function renderListBrew(obj) {
-        const list = document.querySelector("div#menu-container");
-        const caption = document.createElement("div");
-        caption.className = "caption";
-
-        const item = document.createElement("div");
-        item.className = "menu-block";
-        item.id = obj.id
-
-        const name = document.createElement("div");
-        name.className = "title";
-        name.innerHTML = obj.name;
-
-        const subtitle = document.createElement("div");
-        subtitle.className = "subtitle";
-        subtitle.innerHTML = `${obj.city}, ${obj.state}`;
-
-        caption.append(name, subtitle);
-        item.append(caption);
-        item.classList.add('menu-block');
-        list.append(item);
-
-        const location = {lat: parseFloat(obj.latitude), lng: parseFloat(obj.longitude)}
-
-        let marker = new google.maps.Marker({
-            title: obj.name,
-            position: location,
-            map: map
-        })
-
-        markers.push(marker)
-
-<<<<<<< Updated upstream
-        //when user clicks on a search result, populate details section
-        //also pan to result in the map view
-        item.addEventListener("click", fetchBrewDetails)
-
-=======
-        item.addEventListener("click", (e) => {
-            console.log('ding')
-            const p = document.createElement('p')
-            p.textContent = e.target.innerText
+    if (!!document.querySelector('form#search').searchToggle[1].checked) {
+        //prepare to search for breweries
+        e.preventDefault();
+        let cityName = e.target[2].value;
+        let cityLower = cityName.toLowerCase();
+        let cityArr = cityLower.split(" ");
+        let cityUrl = cityArr.join("_");
+        const cityFetch = fetch(BREW_URL + `?by_city=${cityUrl}`).then(r => r.json());
         
-            document.querySelector('div#details').appendChild(p)
-        })
->>>>>>> Stashed changes
+        clearList();
+        removeMarkers();
+        cityFetch.then(o => o.forEach(renderListBrew))
+        cityFetch.then(o => {
+                let lat = parseFloat(o[0].latitude);
+                let lng = parseFloat(o[0].longitude);
+                console.log(lat, lng);
+                let location = {lat: lat, lng: lng};
+                map.panTo(location)
+            })
+    } else {
+        //prepare to search for beers
+        e.preventDefault();
+        let beerName = e.target[2].value;
+        let beerLower = beerName.toLowerCase();
+        let beerArr = beerLower.split(" ");
+        let beerUrl = beerArr.join("_");
+
+        const beerFetch = fetch(BEER_URL + `?beer_name=${beerUrl}`).then(r => r.json());
+        console.log(beerFetch)
+        clearList();
+        removeMarkers();
+        beerFetch.then(o => o.forEach(renderListBeer))
     }
+}
 
-    function searchHandler() {
-        let searchBar = document.querySelector("form");
-        console.log(searchBar)
-        searchBar.addEventListener("submit", (e) => {
-            e.preventDefault();
-            let cityName = e.target[2].value;
-            let cityLower = cityName.toLowerCase();
-            let cityArr = cityLower.split(" ");
-            let cityUrl = cityArr.join("_");
-            const cityFetch = fetch(BREW_URL + `?by_city=${cityUrl}`).then(r => r.json());
-            
-            clearList();
-            removeMarkers();
-            cityFetch.then(o => o.forEach(renderListBrew))
-            cityFetch.then(o => {
-                    let lat = parseFloat(o[0].latitude);
-                    let lng = parseFloat(o[0].longitude);
-                    console.log(lat, lng);
-                    let location = {lat: lat, lng: lng};
-                    map.panTo(location)
-                })
-        })
-    }
+//used to fetch add'l details for content section
+function fetchBrewDetails(e) {
+    console.log(e.target)
+    fetch(BREW_URL + `/${e.target.id}`)
+    .then(res => res.json())
+    .then(obj => {
+        const name = document.querySelector('#detail-name')
+        name.textContent = obj.name
 
-    function formSwapHandler(e){
-        console.log(e.target.id)
+        const type = document.querySelector('#detail-type')
+        type.textContent = obj.brewery_type
 
-        // const beer = document.querySelector("#beer-search")
-        // const brew = document.querySelector("#brew-search")
+        const address = document.querySelector('#detail-address')
+        address.textContent = obj.street
 
-        // beer.addEventListener("click", (e) => {
-        //     clearBody();
-        //     fetchBeerContent();
-        // })
+        const cityState = document.querySelector('#detail-city-state')
+        cityState.textContent = `${obj.city}, ${obj.state}`
 
-        // brew.addEventListener("click", (e) => {
-        //     clearBody();
-        // })
-    }
+        const phone = document.querySelector('#detail-phone')
+        phone.textContent = obj.phone
 
-    document.querySelectorAll('.search-filter').addEventListener("click", formSwapHandler)
+        const website = document.querySelector('#detail-website')
+        website.textContent = obj.website_url
 
-    function clearBody() {
-        const bodyContent = document.querySelector("div#content-section");
-        while(bodyContent.firstChild) {bodyContent.removeChild(bodyContent.firstChild)};
-    }
-
-    function clearList() {
-        const listContent = document.querySelector("div#menu-container");
-        while(listContent.firstChild) {listContent.removeChild(listContent.firstChild)};
-    }
-
-<<<<<<< Updated upstream
-    function removeMarkers() {
-        for (i=0; i<markers.length; i++) {
-            markers[i].setMap(null)
+        try { 
+            map.panTo({lat: parseFloat(obj.latitude), lng: parseFloat(obj.longitude)})
+        } catch {
+            alert('No geolocation data available for this brewery.')
         }
-    }
-
-=======
-
-
-/////////////////
-    [...document.querySelectorAll('.menu-block')].forEach((obj) => {
-        obj.addEventListener('click', (e) => {
-
-        })
+        
     })
->>>>>>> Stashed changes
+}
+
+function formSwapHandler(e){
+    clearList()
+
+    if (e.target.value === 'beer') {
+        //hide the map
+        document.querySelector('#map').style.display = 'none'
+    } else {
+        //bring the map back
+        document.querySelector('#map').style.display = 'block'
+    }
+}
+
+function clearBody() {
+    const bodyContent = document.querySelector("div#content-section");
+    while(bodyContent.firstChild) {bodyContent.removeChild(bodyContent.firstChild)};
+}
+
+function clearList() {
+    const listContent = document.querySelector("div#menu-container");
+    // console.log(listContent)
+    while(listContent.firstChild) {listContent.removeChild(listContent.firstChild)};
+}
+
+function removeMarkers() {
+    for (i=0; i<markers.length; i++) {
+        markers[i].setMap(null)
+    }
+}
